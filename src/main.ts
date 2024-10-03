6f5e8422e005697e56d17bfddf859cd9b020759d
danielseduc <daniel@seduc.net>
2024-10-03 15:31:14 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Autenticador de usuários com JWT - Daniel Marques')
+    .setDescription('API para autenticar usuários através de tokens de sessão')
+    .setVersion('1.0')
+    .addTag('Usuários')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
